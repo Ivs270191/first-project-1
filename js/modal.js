8cf7e09f5e3e5ac03dd88b61a7ae2e17bbd05419
Ivs270191 <152504971+Ivs270191@users.noreply.github.com>
2024-03-23 13:26:44 +0200

    modal add

--- js/modal.js
+++ js/modal.js
@@ -1,8 +1,8 @@
-const modal = document.querySelector('.modal');
+const modal = document.querySelector('.modal-background');
 const modalBtnOpen = document.querySelector('.modal-btn-open');
-const modalBtnClose = document.querySelector('.modal-btn-close');
+const modalBtnClose = document.querySelector('.modal__close-btn');
 
-const toggleModal = () => { modal.classList.toggle('is-hidden') };
+const toggleModal = () => modal.classList.toggle('is-hidden');
 
-modalBtnOpen.addEventListener('Click', toggleModal);
-modalBtnClose.addEventListener('Click', toggleModal);
+modalBtnOpen.addEventListener('click', toggleModal);
+modalBtnClose.addEventListener('click', toggleModal);
