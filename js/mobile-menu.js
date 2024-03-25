1dd77ad39ad4067f83d70b3953d7c8a77bae9f31
Ivs270191 <152504971+Ivs270191@users.noreply.github.com>
2024-03-25 17:33:37 +0200

    some correctioins with mobile menu

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,8 +1,16 @@
 const mobileMenu = document.querySelector('.mobile-menu');
 const mobileBtnOpen = document.querySelector('.mobile-button');
-const mobileBtnClose = document.querySelector('.mobile-menu__close-btn');
+const mobileBtnClose = document.querySelector('.mobile-menu__close-btn')
+const mobileBtnClose1 = document.querySelector(".mob-close1");
+const mobileBtnClose2 = document.querySelector(".mob-close2");
+const mobileBtnClose3 = document.querySelector(".mob-close3");
+const mobileBtnClose4 = document.querySelector(".mob-close4");
 
 const toggleMobile = () => mobileMenu.classList.toggle('outpage');
 
 mobileBtnOpen.addEventListener('click', toggleMobile);
 mobileBtnClose.addEventListener('click', toggleMobile);
+mobileBtnClose1.addEventListener('click', toggleMobile);
+mobileBtnClose2.addEventListener('click', toggleMobile);
+mobileBtnClose3.addEventListener('click', toggleMobile);
+mobileBtnClose4.addEventListener('click', toggleMobile);
