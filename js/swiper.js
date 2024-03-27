63465b398c818bea3c5a54fe0bba28d666dfbd45
Ivs270191 <152504971+Ivs270191@users.noreply.github.com>
2024-03-27 14:34:36 +0200

    swiper add

---
+++ js/swiper.js
@@ -0,0 +1,31 @@
+const swiper = new Swiper('.swiper-container', {
+    loop: true,
+    autoplay: {
+        delay: 5000,
+    },
+    spaceBetween: 20,
+    slidesPerView: 1,
+    breakpoints: {
+        768: {
+            slidesPerView: 2,
+        },
+
+        1280: {
+            slidesPerView: 3,
+        },
+
+    },
+
+    pagination: {
+        el: '.pagination',
+        bulletClass: 'pagination__button',
+        bulletActiveClass: 'pagination__button--active',
+    },
+
+    navigation: {
+        nextEl: '.arrow-button__after',
+        prevEl: '.arrow-button',
+
+    }
+
+});
